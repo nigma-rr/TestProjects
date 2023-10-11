2474e6497e281931f765420ed7b07c577eb0cad7
Radmir <r.nigmatullin@mail.business.ru>
2023-10-11 05:30:49 +0500

    rebuild

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  assetsDir: 'assets',
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
   outputDir: 'docs',
   productionSourceMap: false,
 })
